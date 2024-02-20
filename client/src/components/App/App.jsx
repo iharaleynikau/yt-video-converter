8fd081877fadacbe73b0931574181467f0f04a7c
Igor Aleynikov <iharaleynikov@gmail.com>
2024-02-20 15:28:47 +0300

    basic functional

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,7 +1,53 @@
-import './App.css'
+import { useState } from 'react';
+import { Input, Button, Progress } from 'antd';
+import { LinkOutlined, DownloadOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import './App.css';
 
 const App = () => {
-  return <div className="App">Hi</div>
-}
+  const [link, setLink] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-export default App
+  const onButtonClick = async () => {
+    setIsLoading(true);
+    await axios
+      .post(
+        'http://localhost:3300/convert',
+        {
+          link
+        },
+        {
+          responseType: 'blob',
+          onUploadProgress: progressEvent => {
+            setProgress(Math.round((100 * progressEvent.loaded) / progressEvent.total));
+          }
+        }
+      )
+      .then(res => {
+        console.log(res);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <div className="main-wrapper">
+      <div className="content-wrapper">
+        <h1 className="mb-15">Provide an YT-link</h1>
+        <Input
+          value={link}
+          onChange={event => setLink(event.target.value)}
+          className="mb-15"
+          size="medium"
+          prefix={<LinkOutlined />}
+        />
+        {isLoading && <Progress size="small" type="circle" percent={progress} className="mb-15" />}
+        <Button loading={isLoading} onClick={onButtonClick} type="primary" icon={<DownloadOutlined />}>
+          Convert and download
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import { dirname, resolve, join } from 'path';
+import ytdl from 'ytdl-core';
+import { createWriteStream } from 'fs';
 import 'dotenv/config';
 
 const app = express();
@@ -18,6 +20,28 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
+
+app.post('/convert', async (req, res) => {
+  const link = req.body.link;
+
+  const title = await ytdl.getInfo(link).then(info => {
+    return info.videoDetails.title;
+  });
+
+  ytdl(link, {
+    quality: 'highestaudio',
+    filter: 'audioonly'
+  }).pipe(
+    createWriteStream(`${title}.mp3`.split('/').join(' '))
+      .on('error', err => {
+        console.log(err);
+        res.send('Error: ' + err.message);
+      })
+      .on('finish', () => {
+        res.status(200).send('Downloaded!');
+      })
+  );
+});
 
 const PORT = process.env.PORT || 3300;
 
